161dfb26a81d852abd937d7b16dd2597c14759aa
Erik Larsson <erik.larsson@musiker.nu>
2015-07-05 20:36:54 +0200

    Minor console changes

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/dist'));
-console.log('web path: ' + __dirname + '/dist');
 
 app.get('/', function(req, res) {
-    console.log('hello there');
-    res.sendFile('./dist/index.html'); // load our public/index.html file
+    res.sendFile(__dirname + '/dist/index.html'); // load our public/index.html file
 });
 
 var port = process.env.PORT || 3000;
